fe1c33d37d7e4f9faba49c825bda21ba8734c33e
Zzzz0125 <85991971+Zzzz0125@users.noreply.github.com>
2023-11-13 14:58:45 +0800

    Update and rename app合集.js to app.js

--- app.js
+++ app.js
@@ -12,12 +12,6 @@
 ^https:\/\/api\.hummingbird\.businessreview\.global\/api\/subscriptions\/get_active url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/jjxrsl.js
 hostname=api.hummingbird.businessreview.global
 
-# 牛津高阶词典第十版
-^https:\/\/oxfordx\.cp\.com\.cn\/api\/pay\/apple_notify url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/niujin10.js
-# 牛津高阶词典-去除首页下方广告
-^https:\/\/oxadmin\.cp\.com\.cn\/api\/(hot\/index|advertise\/banner) url reject-dict
 
-
-  
 [mitm]
-hostname=api.hechuangxinxi.xyz, jdforrepam.com,api.hummingbird.businessreview.global,oxfordx.cp.com.cn
+hostname=api.hechuangxinxi.xyz,jdforrepam.com,api.hummingbird.businessreview.global
